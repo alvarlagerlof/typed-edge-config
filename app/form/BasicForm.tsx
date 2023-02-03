78859d9413db655c6018e80eded60db42e5978ae
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-02-03 02:36:24 +0100

    style

--- app/form/BasicForm.tsx
+++ app/form/BasicForm.tsx
@@ -8,9 +8,12 @@ const schema = z
   .object({
     firstName: z.string(),
     lastName: z.string(),
-    email: z.string().email(),
+    email: z.string().email().describe("Has to be valid!!"),
     age: z.number().min(18).describe("Has a limit"),
     fulfilled: z.boolean(),
+    favoriteDotComSite: z
+      .string()
+      .endsWith(".com", { message: "Only .com domains allowed" }),
     nested: z.object({
       foo: z.string().describe("This is a description!"),
       bar: z.string(),
--- app/form/page.tsx
+++ app/form/page.tsx
@@ -2,9 +2,11 @@ import { BasicForm } from "./BasicForm";
 
 export default function PageForm() {
   return (
-    <div className="p-20 flex flex-col gap-8">
-      <h1 className="text-xl font-medium">Zod to form</h1>
-      <BasicForm />
+    <div className="flex flex-col items-center">
+      <div className="p-20 flex flex-col gap-8 max-w-3xl w-full">
+        <h1 className="text-xl font-medium">Zod to form</h1>
+        <BasicForm />
+      </div>
     </div>
   );
 }
